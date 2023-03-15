b9bb44c2ccef00ed0efaa9d36b36bf2e86f56bfb
BRIAN BETT <+>
2023-03-15 21:11:13 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply=num1*num2;
+console.log(multiply);
+let random= 7 ;
+Math.random();
+console.log(random);
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod);
+
+let num = (14,15,16,17,18,19,20);
+let max = Math.max(15,16,17,18,19,20);
+console.log(max);
